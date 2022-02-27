77d87af18f77dc6d5d77b11bda211066a910e5af
tajoutihamza <tajouti.hamza@gmail.com>
2022-02-27 23:33:18 +0100

    update name and email of the student using ID and put throw @PutMapping and @Transactional

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -35,11 +36,19 @@ public class StudentService {
         }
         studentRepository.deleteById(studentId);
     }
-//    @Transactional
-//    public void updateStudent(Long studentId) {
-//        boolean exists = studentRepository.existsById(studentId);
-//        if(!exists){
-//            throw new IllegalStateException("Student with "+studentId+" does not exists! add it first");
-//        }
-//    }
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId).orElseThrow(()->new  IllegalStateException("Student with "+studentId+" does not exists! add it first"));
+
+        if(name!=null && name.length()>0&& !Objects.equals(name,student.getName())){
+            student.setName(name);
+        }
+        if(email!=null && email.length()>0&& !Objects.equals(email,student.getEmail())){
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+            if(studentOptional.isPresent()){
+                throw new IllegalStateException("Email Taken");
+            }
+            student.setEmail(email);
+        }
+    }
 }
